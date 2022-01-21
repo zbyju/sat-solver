e527d60417541ab2beb1a980ef0b8161ba44a4e0
Zbynek Jurica <zbynek.jurica@gmail.com>
2022-01-21 16:46:12 +0100

    Change print instance

--- src/main/scala/model/instance/Instance.scala
+++ src/main/scala/model/instance/Instance.scala
@@ -3,10 +3,11 @@ package model.instance
 import model.variable.Variable
 import model.clause.Clause
 import model.`type`.Bool
+import model.solution.Solution
 
-class Instance(id: Int, setName: String, variables: Seq[Variable], clauses: Seq[Clause]) {
+class Instance(id: Int, setName: String, variables: Seq[Variable], clauses: Seq[Clause], solution: Solution) {
   def solve(): Bool = clauses.map(c => c.value).forall(c => c.value)
 
 
-  override def toString = s"I ($setName - $id) - $variables: ${clauses.mkString("\n", "\n * ", "\n")}"
+  override def toString = s"\nI ($setName - $id) - $variables: ${clauses.mkString("\n", "\n * ", "\n")} (sol: $solution)\n"
 }
