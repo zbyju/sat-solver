d2d8b07b259837ca74b459c787e1487b2eb8fc3c
Zbynek Jurica <zbynek.jurica@gmail.com>
2022-01-21 16:47:26 +0100

    Add stats tracker

---
+++ src/main/scala/stats/StatsTracker.scala
@@ -0,0 +1,19 @@
+package stats
+
+case class StatsTracker() {
+  private var timeStart: Long = _
+  private var timeEnd: Long = _
+
+  def getTimeStart: Long = timeStart
+
+  def setTimeStart(): Unit = timeStart = System.nanoTime()
+  def setTimeStart(time: Long): Unit = timeStart = time
+
+  def setTimeEnd(): Unit = timeEnd = System.nanoTime()
+
+  def getTimeNano: Long = timeEnd - timeStart
+
+  override def toString(): String = {
+    s"""Time: $getTimeNano""".stripMargin
+  }
+}
