8c7eebcc8bc5ee50f7070da16e7501c97950a054
Zbynek Jurica <zbynek.jurica@gmail.com>
2022-01-22 19:10:42 +0100

    Change instance printing to have colors

--- src/main/scala/model/instance/Instance.scala
+++ src/main/scala/model/instance/Instance.scala
@@ -5,12 +5,16 @@ import model.solution.Solution
 import model.variable.Variable
 import model.instance.Instance
 import model.instance.Configuration
+import io.AnsiColor._
 
 class Instance(val id: Int, val setName: String,
                override val variables: Seq[Variable],
                override val clauses: Seq[Clause],
                val solution: Solution)
-                      extends Configuration(variables, clauses)
+                      extends Configuration(variables, clauses) {
+
+  override def toString = s"${GREEN}$setName (id=$id}):${RESET} $variables \n $clauses \n $solution"
+}
 
 object Instance {
   def createConfiguration(i: Instance): Configuration = new Configuration(i.variables, i.clauses)
