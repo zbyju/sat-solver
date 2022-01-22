9f26addfdee124bf2c9a0b449d2c9f581c53f782
Zbynek Jurica <zbynek.jurica@gmail.com>
2022-01-22 19:11:43 +0100

    Add result printing

--- src/main/scala/model/result/Result.scala
+++ src/main/scala/model/result/Result.scala
@@ -1,8 +1,17 @@
 package model.result
 
+import model.solution.Solution
 import stats.StatsTracker
 
-class Result(val sum: Int, isSolution: Boolean,
-             val valuation: Seq[Boolean], val statsTracker: StatsTracker) {
-  statsTracker.setTimeStartIfNotSet()
+import io.AnsiColor.*
+
+class Result(val id: Int, val setName: String, val sum: Int, isSolution: Boolean,
+             val evaluation: Seq[Boolean], val exactSolution: Option[Solution], val statsTracker: StatsTracker) {
+  statsTracker.setTimeEnd()
+
+
+  override def toString = exactSolution match {
+    case Some(s) => s"${BLUE}RESULT - $setName (id=$id)${RESET}\nSUM: $sum ($isSolution) vs ${s.sum} (true)\n$evaluation\n${s.evaluation}"
+    case None => s""
+  }
 }
