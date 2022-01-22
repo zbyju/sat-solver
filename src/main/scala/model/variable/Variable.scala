10633749ec5536952287eec6435a2cdb42a6a84b
Zbynek Jurica <zbynek.jurica@gmail.com>
2022-01-22 19:12:55 +0100

    Add implementation to Variable

--- src/main/scala/model/variable/Variable.scala
+++ src/main/scala/model/variable/Variable.scala
@@ -1,9 +1,13 @@
 package model.variable
 
+import io.AnsiColor._
 import model.variable.Variable
 
-import scala.language.implicitConversions
-
 class Variable(val index: Int, val weight: Int, val value: Boolean = false) {
-  override def toString = s"x$index ($weight) = $value"
+  def unary_! = new Variable(index, weight, !value)
+  def changeValue(newValue: Boolean): Variable = new Variable(index, weight, newValue)
+
+  def valueInt: Int = if(value) 1 else 0
+
+  override def toString = s"${BOLD}x$index${RESET}(W=$weight)=$valueInt"
 }
