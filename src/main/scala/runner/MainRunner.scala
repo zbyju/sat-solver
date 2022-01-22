cb65eed478f3645cd9585a1c4f0851e563bb3fec
Zbynek Jurica <zbynek.jurica@gmail.com>
2022-01-22 19:11:02 +0100

    Add methods to run solver

--- src/main/scala/runner/MainRunner.scala
+++ src/main/scala/runner/MainRunner.scala
@@ -1,13 +1,24 @@
 package runner
 
 import file.MFileLoader
+import model.instance.Instance
+import solver.simulatedannealing.{SAConfig, SASolver}
+import stats.StatsTracker
 
 object MainRunner {
   val loader = new MFileLoader()
+  val solver = new SASolver(new SAConfig(0.9, 40, 25))
+
+  def runInstance(i: Instance, solver: SASolver = solver): Unit = {
+    val res = solver.solve(i, StatsTracker())
+    println(res)
+  }
+
+  def runSet(instances: Seq[Instance], solver: SASolver = solver): Unit = {
+
+  }
 
   def main(args: Array[String]): Unit = {
-    println(loader.filenames2078.length)
-    println(loader.solutionLines2078.length)
-    println(loader.instances2078.length)
+    runInstance(loader.instances2078.head)
   }
 }
