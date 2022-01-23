80590deac81fa6e7be561a8370e853ad2f508d08
Zbynek Jurica <zbynek.jurica@gmail.com>
2022-01-23 17:38:38 +0100

    Add tracking of visited states + stats

--- src/main/scala/stats/StatsTracker.scala
+++ src/main/scala/stats/StatsTracker.scala
@@ -4,6 +4,7 @@ case class StatsTracker() {
   private var timeStart: Long = -1
   private var timeEnd: Long = _
   private var progression: Seq[Int] = Seq()
+  private val visited = scala.collection.mutable.HashMap.empty[Seq[Boolean], Int]
 
   def getTimeStart: Long = timeStart
 
@@ -17,6 +18,18 @@ case class StatsTracker() {
 
   def addProgress(p: Int): Unit = progression = progression :+ p
   def getProgression: Seq[Int] = progression
+  def addVisited(node: Seq[Boolean]) = {
+    val count = visited.getOrElse(node, 0)
+    visited += (node -> (count + 1))
+  }
+
+  def getAverageVisited: Double = visited.values.sum.toDouble / visited.size.toDouble
+
+  def getCountVisited: Int = visited.size
+
+  def getCoverVisited(maxCount: Int): Double = visited.size.toDouble / maxCount.toDouble
+
+  def getMaxVisited: Double = visited.values.max
 
   override def toString: String = {
     s"""Time: $getTimeNano""".stripMargin
