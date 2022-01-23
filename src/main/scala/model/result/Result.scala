9f43602ce3f1fc9844aed112a1fdd28dcd7b951e
Zbynek Jurica <zbynek.jurica@gmail.com>
2022-01-23 17:37:23 +0100

    Add nice result printing

--- src/main/scala/model/result/Result.scala
+++ src/main/scala/model/result/Result.scala
@@ -9,8 +9,17 @@ class Result(val id: Int, val setName: String, val sum: Int, isSolution: Boolean
              val evaluation: Seq[Boolean], val exactSolution: Option[Solution], val statsTracker: StatsTracker) {
   statsTracker.setTimeEnd()
 
+  def comparisonIcon: String = exactSolution match {
+    case Some(s) => if(s.sum == sum) s"${GREEN}✔${RESET}" else if(s.sum > sum) s"${RED}❌${RESET}" else s"${CYAN}?${RESET}"
+    case None => s"${CYAN}?${RESET}"
+  }
 
-  override def toString = exactSolution match {
+  def toStringCompare: String = exactSolution match {
+    case Some(s) => s"${BLUE}RESULT - $setName (id=$id)${RESET} - $sum ($isSolution) vs ${s.sum} (true) " + comparisonIcon
+    case None => s"${BLUE}RESULT - $setName (id=$id)${RESET} - $sum ($isSolution) vs No exact solution " + comparisonIcon
+  }
+
+  override def toString: String = exactSolution match {
     case Some(s) => s"${BLUE}RESULT - $setName (id=$id)${RESET}\nSUM: $sum ($isSolution) vs ${s.sum} (true)\n$evaluation\n${s.evaluation}"
     case None => s""
   }
