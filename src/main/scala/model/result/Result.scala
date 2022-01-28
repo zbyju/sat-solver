9435298d75804bb4ffa2c3e644cc5551fef9e31c
Zbynek Jurica <zbynek.jurica@gmail.com>
2022-01-28 18:01:38 +0100

    Add function to calculate deviation

--- src/main/scala/model/result/Result.scala
+++ src/main/scala/model/result/Result.scala
@@ -3,6 +3,8 @@ package model.result
 import model.solution.Solution
 import stats.StatsTracker
 
+import scala.math.{abs, max}
+
 import io.AnsiColor.*
 
 class Result(val id: Int, val setName: String, val sum: Int, isSolution: Boolean,
@@ -12,6 +14,11 @@ class Result(val id: Int, val setName: String, val sum: Int, isSolution: Boolean
   def comparisonIcon: String = exactSolution match {
     case Some(s) => if(s.sum == sum) s"${GREEN}✔${RESET}" else if(s.sum > sum) s"${RED}❌${RESET}" else s"${CYAN}?${RESET}"
     case None => s"${CYAN}?${RESET}"
+  }
+
+  def getDeviation: Double = exactSolution match {
+    case Some(e) => abs(sum - e.sum).toDouble / max(sum, e.sum).toDouble
+    case None => 0
   }
 
   def toStringCompare: String = exactSolution match {
