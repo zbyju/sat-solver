1d5cc8673985e9d97a58a1d1ca0977ed9c5df32a
Zbynek Jurica <zbynek.jurica@gmail.com>
2022-01-28 18:01:24 +0100

    Add main runner

--- src/main/scala/runner/MainRunner.scala
+++ src/main/scala/runner/MainRunner.scala
@@ -1,24 +1,35 @@
 package runner
 
-import file.MFileLoader
+import file.{FileSaver, MFileLoader}
 import model.instance.Instance
+import model.result.Result
 import solver.simulatedannealing.{SAConfig, SASolver}
 import stats.StatsTracker
 
 object MainRunner {
   val loader = new MFileLoader()
-  val solver = new SASolver(new SAConfig(0.9, 40, 25))
+  val saver = new FileSaver()
+  val solver = new SASolver(new SAConfig())
 
-  def runInstance(i: Instance, solver: SASolver = solver): Unit = {
+  def runInstance(i: Instance, solver: SASolver = solver): Result = {
     val res = solver.solve(i, StatsTracker())
-    println(res)
+    print(res.toStringCompare)
+    println(s" | ${res.statsTracker.getAverageVisited} / ${res.statsTracker.getMaxVisited} / ${res.statsTracker.getCoverVisited(scala.math.pow(2, i.variables.length).toInt) * 100}")
+    res
   }
 
-  def runSet(instances: Seq[Instance], solver: SASolver = solver): Unit = {
-
+  def runSet(instances: Seq[Instance], solver: SASolver = solver): Seq[Result] = {
+    instances.map(i => runInstance(i, solver))
   }
 
   def main(args: Array[String]): Unit = {
-    runInstance(loader.instances2078.head)
+//    var res = runInstance(loader.instances2078.head)
+//    saver.saveProgression(res, "TEST")
+//
+//    res = runInstance(loader.instances2078(1))
+//    saver.saveProgression(res, "TEST2")
+
+    val res = runSet(loader.instances2078)
+    saver.saveAverages(res, solver.name)
   }
 }
