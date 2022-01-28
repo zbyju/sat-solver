82334f2f8d83b615c9143caf3fc696df2fc4d85c
Zbynek Jurica <zbynek.jurica@gmail.com>
2022-01-28 18:02:46 +0100

    Add add progress to stats tracker

--- src/main/scala/stats/StatsTracker.scala
+++ src/main/scala/stats/StatsTracker.scala
@@ -3,7 +3,8 @@ package stats
 case class StatsTracker() {
   private var timeStart: Long = -1
   private var timeEnd: Long = _
-  private var progression: Seq[Int] = Seq()
+  private var progression: Seq[Double] = Seq()
+  private var progressionCost: Seq[Double] = Seq()
   private val visited = scala.collection.mutable.HashMap.empty[Seq[Boolean], Int]
 
   def getTimeStart: Long = timeStart
@@ -16,8 +17,12 @@ case class StatsTracker() {
 
   def getTimeNano: Long = timeEnd - timeStart
 
-  def addProgress(p: Int): Unit = progression = progression :+ p
-  def getProgression: Seq[Int] = progression
+  def addProgress(p: Double): Unit = progression = progression :+ p
+  def getProgression: Seq[Double] = progression
+
+  def addProgressCost(p: Double): Unit = progressionCost = progressionCost :+ p
+  def getProgressionCost: Seq[Double] = progressionCost
+
   def addVisited(node: Seq[Boolean]) = {
     val count = visited.getOrElse(node, 0)
     visited += (node -> (count + 1))
