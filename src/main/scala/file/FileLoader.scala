ce1d9ceea68688da1d6630604807f0096f5703c7
Zbynek Jurica <zbynek.jurica@gmail.com>
2022-01-30 17:37:42 +0100

    Add fields for inheritance

--- src/main/scala/file/FileLoader.scala
+++ src/main/scala/file/FileLoader.scala
@@ -1,9 +1,21 @@
 package file
 
+import model.instance.Instance
+
 import java.io.File
 import scala.io.{BufferedSource, Source}
 
+type SolutionWithId = (Int, String)
+type FilenamesWithId = (Int, String)
+
 class FileLoader {
+  val name: String = "F"
+  val name1: String = "1"
+  val name2: String = "2"
+
+  val instances1: Seq[Instance] = Seq()
+  val instances2: Seq[Instance] = Seq()
+
   def getInputFile(path: String): BufferedSource = {
     Source.fromURL(getClass.getResource(path))
   }
